30f17b719da578efef9834a82e028ea8fd0babac
firas <firas.mosbehi@insat.ucar.tn>
2024-04-14 14:57:51 +0100

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <div style={{
+            backgroundImage: `url('/home.jpg')`,
+            backgroundSize: 'cover', /* Adjust background size as needed */
+            backgroundPosition: 'center', /* Adjust background position as needed */
+            width: '100vw', /* Optional: Set width for full viewport width */
+            height: '100vh', /* Optional: Set height for full viewport height */
+        }} className="flex flex-col items-center justify-end gap-4 pb-20">
+            <strong className="text-[24px]">Explore Tunisia{"'"}s ancient wonders</strong>
+            <p className="text-[20px]">Step into history with VR tourism and join us now , a lot of fun is waiting !</p>
+            <button className="border border-solid border-white text-[20px] mt-12 font-semibold px-4 py-2 rounded-2xl">Explorer</button>
+        </div>
+      </main>
+  );
+}
---
+++ src/app/tours/[id]/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="relative">
+      <video width="1500" height="800" muted loop={true} autoPlay={true}>
+        <source src="/tours/el jam amphitheatre.mp4" type="video/mp4" />
+      </video>
+      <div className="absolute top-60 flex flex-col gap-8 items-center left-[30%] max-w-[800px]">
+        <strong className="text-[48px] text-center font-bold">
+          Explorer l{"'"}amphitheatre de Jam
+        </strong>
+        <p className="text-[28px] text-center max-w-[600px]">
+          Décvouvrer l{"'"}un des grands chefs d{"'"}
+          oeuvres des Romains
+        </p>
+        <button className="px-4 py-2 mt-8 border border-white border-solid rounded-2xl">
+          Visiter le monument
+        </button>
+      </div>
+    </div>
+  );
+}
